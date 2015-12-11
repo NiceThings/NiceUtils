83c0659aa83b31494b8066dca69edad05ddd54af
mattthousand <matt.buckley@raizlabs.com>
2015-12-11 17:37:15 -0500

    Renaming.

--- Examples/Examples/SplitAnimationController-Example/ViewController.swift
+++ Examples/Examples/SplitAnimationController-Example/ViewController.swift
@@ -11,7 +11,7 @@ import MattUtils
 
 class ViewController: UITableViewController {
 
-    var currentTransition: SplitTransitionController?
+    var currentTransition: SplitTransition?
     var currentCell: UITableViewCell?
 
     let colors = [
@@ -68,7 +68,7 @@ extension ViewController: UINavigationControllerDelegate {
     func navigationController(navigationController: UINavigationController, animationControllerForOperation operation: UINavigationControllerOperation, fromViewController fromVC: UIViewController, toViewController toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
 
         if (operation == .Push && fromVC == self) {
-            let splitTransition = SplitTransitionController()
+            let splitTransition = SplitTransition()
             splitTransition.transitionDuration = 2.0
             splitTransition.transitionType = .Push
             splitTransition.splitLocation = currentCell != nil ? CGRectGetMidY(currentCell!.frame) : CGRectGetMidY(view.frame)
